004c9143ecb2ed0b28ed638efacb14cb9385e69b
AbhinavDusi <46360677+AbhinavDusi@users.noreply.github.com>
2021-12-21 11:10:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "NeuralNet.hpp"
 
 int main() {
-    std::vector<int> topology(4); 
+    std::vector<int> topology; 
     topology.push_back(2); 
     topology.push_back(4); 
     topology.push_back(1); 
@@ -15,30 +15,32 @@ int main() {
     std::mt19937 rng(time(nullptr));
 
     for (int i = 0; i < 2000; i++) {
-        int a = (rng()/(double) rng.max())%2; 
-        int b = (rng()/(double) rng.max())%2; 
+        int a = rng()%2; 
+        int b = rng()%2;
 
-        std::vector<double> input(2); 
+        std::vector<double> input; 
         input.push_back(a); 
         input.push_back(b); 
 
-        std::vector<double> target(1); 
-        target.push_back(a^b); 
+        std::vector<double> target; 
+        target.push_back((a^b)); 
 
         net.feed_forward(input);
         net.back_prop(target); 
     }
 
-    int a = (rng()/(double) rng.max())%2; 
-    int b = (rng()/(double) rng.max())%2;
+    int a = rng()%2;
+    int b = rng()%2;
 
-    std::vector<double> input(2); 
+    std::vector<double> input; 
     input.push_back(a); 
     input.push_back(b); 
 
+    net.feed_forward(input);
+
     std::vector<double> result = net.get_result(); 
 
-    std::cout << a << " XOR " << b << " = " result.front() << std::endl; 
+    std::cout << a << " XOR " << b << " = " << result.front() << std::endl; 
 
     return 0; 
 }
